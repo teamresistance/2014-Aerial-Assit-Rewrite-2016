4f363b195cbd2ca70d4715796a23d166e0433733
098tarik <tarik098@yahoo.com>
2017-01-16 16:20:22 -0500

    Made the Encoder public and added concept for PID
    I also included my personal concept for a PID, not sure if it good or
    not. I think you guys have worked with them in the past so feel free to
    roast me :-)

--- src/main/java/org/teamresistance/frc/IO.java
+++ src/main/java/org/teamresistance/frc/IO.java
@@ -1,12 +1,16 @@
 package org.teamresistance.frc;
 
-import edu.wpi.first.wpilibj.CounterBase;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.SPI;
 import org.strongback.components.Motor;
 import org.strongback.components.ui.FlightStick;
+import org.strongback.control.PIDController;
 import org.strongback.drive.MecanumDrive;
 import org.strongback.hardware.Hardware;
+import org.strongback.mock.MockPIDController;
+
+import edu.wpi.first.wpilibj.CounterBase;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.SPI;
 
 /**
  * @author Shreya Ravi
@@ -38,12 +42,15 @@ public class IO {
   private static final boolean Shooter_Reversed = false;
   // new motor for shooter
   public static final Motor shooterMotor = Hardware.Motors.victorSP(4);
-
+  // init the encoder
+  //Encoder(int aChannel, int bChannel, boolean reverseDirection, CounterBase.EncodingType encodingType)
+  public static  Encoder shooterEncoder = new Encoder(4, 5, Shooter_Reversed, CounterBase.EncodingType.k4X);
+    // using this to possibly implement PID for shooter
+    public static PIDController shooterPID;
+    double p = 0;
+    double i = 0;
+    double d = 0;
   public IO() {
-    // init the encoder
-    //Encoder(int aChannel, int bChannel, boolean reverseDirection, CounterBase.EncodingType encodingType)
-    Encoder shooterEncoder = new Encoder(4, 5, Shooter_Reversed, CounterBase.EncodingType.k4X);
-
     // Math found that could work with shooter encoder, right now im going to put random numbers
     //diamater of the wheel * pi = distance per rotation
     // 1 / ticks per rotation = rotations per tick
@@ -51,5 +58,9 @@ public class IO {
     //diamater of wheel * pi / ricks per rotation = distance per tick
     double distancePerTick = 3 * Math.PI / 200;
     shooterEncoder.setDistancePerPulse(distancePerTick);
+    shooterEncoder.setPIDSourceType(PIDSourceType.kRate);
+
+      shooterPID = new MockPIDController();
+
   }
 }
