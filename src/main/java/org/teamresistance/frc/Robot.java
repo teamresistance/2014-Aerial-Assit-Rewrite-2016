6939b76b4d1c1443e1fb28462f193e6ec873604b
1234vipersting <josephdelcastillo@outlook.com>
2017-02-16 05:44:06 -0500

    Code to test all parts of the controller. The Axis based stuff still needs work though.

--- src/main/java/org/teamresistance/frc/Robot.java
+++ src/main/java/org/teamresistance/frc/Robot.java
@@ -1,5 +1,6 @@
 package org.teamresistance.frc;
 
+import com.sun.org.apache.xpath.internal.functions.FuncFalse;
 import org.strongback.Strongback;
 import org.strongback.SwitchReactor;
 import org.strongback.components.ui.DirectionalAxis;
@@ -16,7 +17,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * @author Rothanak So
  */
 public class Robot extends IterativeRobot {
-  private final Gamepad xboxController = Hardware.HumanInterfaceDevices.xbox360(0);
+  private final Gamepad xboxController = Hardware.HumanInterfaceDevices.xbox360(0); //The 0 indicates that the Controller is in the 1st port
 
   private final Drive drive = new Drive(
       IO.robotDrive,
@@ -26,10 +27,28 @@ public class Robot extends IterativeRobot {
   );
 
 
-  double LeftX = xboxController.getLeftX();
-  double LeftY = xboxController.getLeftY();
+  //double LeftX = xboxController.getLeftX();
+  //double LeftY = xboxController.getLeftY();
 
-  DirectionalAxis dPad_1 = xboxController.getDPad(1);
+  boolean leftBumper = false;
+  boolean leftStick  = false;
+
+  boolean rightBumper = false;
+  boolean rightStick  = false;
+
+  boolean aPressed  = false;
+  boolean bPressed  = false;
+  boolean xPressed  = false;
+  boolean yPressed  = false;
+
+  boolean startPressed  = false;
+  boolean selectPressed = false;
+
+//  DirectionalAxis dPad_1 = xboxController.getDPad(1);
+
+  public Robot() {
+
+  }
 
 
   @Override
@@ -40,6 +59,21 @@ public class Robot extends IterativeRobot {
 
     // Reset the gyro
     reactor.onTriggered(xboxController.getA(), () -> IO.gyro.getNavX().reset());
+
+    reactor.onTriggered( xboxController.getLeftBumper() , () -> leftBumper = true );
+    reactor.onTriggered( xboxController.getLeftStick() , () -> leftStick = true );
+
+    reactor.onTriggered( xboxController.getRightBumper() , () -> rightBumper = true );
+    reactor.onTriggered( xboxController.getRightStick() , () -> rightStick = true );
+
+    reactor.onTriggered( xboxController.getA() , () -> aPressed = true );
+    reactor.onTriggered( xboxController.getB() , () -> bPressed = true );
+    reactor.onTriggered( xboxController.getX() , () -> xPressed = true );
+    reactor.onTriggered( xboxController.getY() , () -> yPressed = true );
+
+    reactor.onTriggered( xboxController.getStart() , () -> startPressed = true );
+    reactor.onTriggered( xboxController.getSelect() , () -> selectPressed = true );
+
   }
 
 
@@ -59,21 +93,23 @@ public class Robot extends IterativeRobot {
     double orientation = IO.gyro.getAngle();
     SmartDashboard.putNumber("Gyro Angle", orientation);
 
-    SmartDashboard.putNumber("Left X", LeftX);
-    SmartDashboard.putNumber("Left Y", LeftY);
-    SmartDashboard.putNumber("Right X", );
-    SmartDashboard.putNumber("Right Y", );
-    SmartDashboard.putNumber("Left Bumper", );
-    SmartDashboard.putNumber("Left Trigger", );
-    SmartDashboard.putNumber("Right Bumper", );
-    SmartDashboard.putNumber("Right Trigger", );
-    SmartDashboard.putNumber("A", );
-    SmartDashboard.putNumber("B", );
-    SmartDashboard.putNumber("X", );
-    SmartDashboard.putNumber("Y", );
-    SmartDashboard.putNumber("Start", );
-    SmartDashboard.putNumber("Start", xboxController.getStart() );
-    SmartDashboard.putNumber("DPad 1", );
+//    SmartDashboard.putNumber("Left X", );
+//    SmartDashboard.putNumber("Left Y", );
+//    SmartDashboard.putNumber("Right X", );
+//    SmartDashboard.putNumber("Right Y", );
+    SmartDashboard.putBoolean("Left Stick", leftStick );
+    SmartDashboard.putBoolean("Right Stick", rightStick);
+    SmartDashboard.putBoolean("Left Bumper", leftBumper );
+    SmartDashboard.putBoolean("Right Bumper", rightBumper);
+//    SmartDashboard.putNumber("Left Trigger", );
+//    SmartDashboard.putNumber("Right Trigger", );
+    SmartDashboard.putBoolean("A", aPressed );
+    SmartDashboard.putBoolean("B", bPressed );
+    SmartDashboard.putBoolean("X", xPressed );
+    SmartDashboard.putBoolean("Y", yPressed );
+    SmartDashboard.putBoolean("Start", startPressed );
+    SmartDashboard.putBoolean("Start", selectPressed );
+//    SmartDashboard.putNumber("DPad 1", );
 
     Feedback feedback = new Feedback(orientation);
     drive.onUpdate(feedback);
