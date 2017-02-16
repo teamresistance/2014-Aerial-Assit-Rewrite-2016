dd39f0bea6fcb7b986441145e2251c7521a21494
1234vipersting <josephdelcastillo@outlook.com>
2017-02-15 22:18:47 -0500

    Added in Strongback Xbox controller and basic drive code. IT WORKS.

--- src/main/java/org/teamresistance/frc/Robot.java
+++ src/main/java/org/teamresistance/frc/Robot.java
@@ -1,12 +1,12 @@
 package org.teamresistance.frc;
 
 import org.strongback.Strongback;
-import edu.wpi.first.wpilibj.IterativeRobot;
 import org.strongback.SwitchReactor;
-import org.strongback.components.ui.Gamepad;
 import org.strongback.hardware.Hardware;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.strongback.components.ui.Gamepad;
+import edu.wpi.first.wpilibj.IterativeRobot;
 import org.teamresistance.frc.subsystem.drive.Drive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Main robot class. Override methods from {@link IterativeRobot} to define behavior.
@@ -15,8 +15,6 @@ import org.teamresistance.frc.subsystem.drive.Drive;
  * @author Rothanak So
  */
 public class Robot extends IterativeRobot {
-  //private final FlightStick leftJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
-  //private final FlightStick rightJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(2);
   private final Gamepad xboxController = Hardware.HumanInterfaceDevices.xbox360(0);
 
   private OpticalFlow opFlow = new OpticalFlow();
@@ -26,7 +24,7 @@ public class Robot extends IterativeRobot {
       IO.robotDrive,
       xboxController.getLeftY(),
       xboxController.getLeftX(),
-      xboxController.getRightTrigger()
+      xboxController.getRightX()
   );
 
   @Override
@@ -35,29 +33,9 @@ public class Robot extends IterativeRobot {
 
     Strongback.configure().recordNoEvents().recordNoData();
     final SwitchReactor reactor = Strongback.switchReactor();
-
-//    // Hold the current angle of the robot while the trigger is held
-//    reactor.onTriggeredSubmit(leftJoystick.getTrigger(), () -> new HoldAngleCommand(drive, IO.gyro.getAngle()));
-//    reactor.onUntriggeredSubmit(leftJoystick.getTrigger(), () -> Command.cancel(drive)); // FIXME doesn't cancel
-//
-//    reactor.onTriggeredSubmit(rightJoystick.getButton(3), () -> new HoldAngleCommand(drive, 135));
-//    reactor.onTriggeredSubmit(rightJoystick.getButton(4), () -> new HoldAngleCommand(drive, 0));
-//
-//    // Cancel ongoing Drive commands. The interrupted commands should hand back operator control
-//    reactor.onTriggered(leftJoystick.getButton(3), () -> {
-//      Strongback.submit(Command.cancel(drive));
-//      drive.setOpenLoop();
-//    });
 //
     // Reset the gyro
     reactor.onTriggered(xboxController.getButton(7), () -> IO.gyro.getNavX().reset());
-//
-//    // Hold angle at 135 or 0 degrees until cancelled or interrupted
-//    reactor.onTriggeredSubmit(rightJoystick.getButton(3), () -> new HoldAngleCommand(drive, 135));
-//    reactor.onTriggeredSubmit(rightJoystick.getButton(4), () -> new HoldAngleCommand(drive, 0));
-
-    //reactor.onTriggeredSubmit(rightJoystick.getButton(11), OpticalFlow::init);
-
   }
 
 
